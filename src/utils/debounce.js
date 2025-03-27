074e78bbfe1580b090c7971d787033373fdf5994
Leonardo <leonardo28rodri@gmail.com>
2025-03-26 22:00:57 -0300

    feat: create a debounce function, but not use yet

---
+++ src/utils/debounce.js
@@ -0,0 +1,9 @@
+function debounce(fn, delay = 500) {
+  let timer
+  return (...args) => {
+    clearTimeout(timer)
+    timer = setTimeout(() => fn(...args), delay)
+  }
+}
+
+export default debounce
