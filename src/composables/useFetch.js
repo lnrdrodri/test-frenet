8cd0fab1924910e5bd35eaf4ee01cb6540e6b245
Leonardo <leonardo28rodri@gmail.com>
2025-03-26 23:01:28 -0300

    feat: implement cache on useFetch composable and create delay as utils functions

--- src/composables/useFetch.js
+++ src/composables/useFetch.js
@@ -1,15 +1,42 @@
 import { ref } from 'vue'
 import { useLoadingStore } from '@/stores/loading'
+import { useCache } from '@/stores/cache'
+import delay from '@/utils/delay'
 
-export async function useFetch(url, options = {}) {
+export async function useFetch(url, options = {}, withCache = true) {
   const data = ref(null)
   const error = ref(null)
   const loading = useLoadingStore()
+  const cache = useCache()
 
   try {
     loading.setIsLoading(true)
+
+    const requestCached = withCache
+      ? cache.hasValidCache({
+          url,
+          options,
+          expiresInMinutes: 1,
+        })
+      : false
+
+    if (requestCached) {
+      await delay(300) // simulate the loading in the UI for the user
+      data.value = requestCached
+      return { data, error }
+    }
+
     const response = await fetch(url, options)
-    data.value = await response.json()
+    const json = await response.json()
+    data.value = json
+
+    if (withCache) {
+      cache.addRequest({
+        url,
+        options,
+        data: json,
+      })
+    }
   } catch (err) {
     error.value = err.message
   } finally {
---
+++ src/stores/cache.js
@@ -0,0 +1,55 @@
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+
+export const useCache = defineStore('cache', () => {
+  const requests = ref([])
+
+  function addRequest({ url, options, data }) {
+    requests.value.push({
+      url,
+      options,
+      data,
+      timestamp: Date.now(),
+    })
+  }
+
+  function isAlreadyCached(url, options) {
+    return (
+      requests.value.find((request) => {
+        return (
+          request.url === url &&
+          JSON.stringify(request.options) === JSON.stringify(options)
+        )
+      }) || false
+    )
+  }
+
+  function isValidCache(cache, expiresInMinutes) {
+    const now = Date.now()
+    const cacheAge = now - cache.timestamp
+    return cacheAge < 1000 * 60 * expiresInMinutes
+  }
+
+  function hasValidCache({ url, options, expiresInMinutes = 5 }) {
+    const requestCached = isAlreadyCached(url, options)
+
+    if (!requestCached) return false
+
+    const cacheIsUpToDate = isValidCache(requestCached, expiresInMinutes)
+
+    if (!cacheIsUpToDate) {
+      clearOldCache()
+      return false
+    }
+
+    return requestCached.data
+  }
+
+  function clearOldCache() {
+    requests.value = requests.value.filter((request) => {
+      return isValidCache(request, 5)
+    })
+  }
+
+  return { requests, addRequest, hasValidCache }
+})
